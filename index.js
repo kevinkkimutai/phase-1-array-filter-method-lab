05b59a9185d51e46a7e4677bc6f4defbabc5898c
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-29 14:38:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(drivers, name) {
+
+    return drivers.filter(findName => {
+        return (findName.toLowerCase() === name.toLowerCase())
+    }
+    )
+}
+
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(findName => {
+        if (letter.charAt(0) === findName.charAt(0)) {
+            return findName
+        }
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(findName => {
+        if (findName.name == name) {
+            return findName
+     
+        }
+    })
+}
